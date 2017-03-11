674d5da7dcebe17ef32d382391f7d73e02229ed6
BunnB <bburapa1@swarthmore.edu>
2017-03-11 15:51:43 -0500

    Update eventaddermanual.py

--- eventaddermanual.py
+++ eventaddermanual.py
@@ -4,16 +4,16 @@ from datetime import *
 from dateutil.relativedelta import *
 from locDic import locDic
 
-firebase = firebase.FirebaseApplication('https://swatevents-2341b.firebaseio.com/',None)
+firebase = firebase.FirebaseApplication('***********************',None)
 
 def adder():
     branch = "approved"
     date = raw_input("Date MM-DD-YYYY:")
-    description = "Test"#raw_input("Desc:")
-    loc = "Clothier"#raw_input("Location:")
-    name = "Test Event"#raw_input("Name:")
-    starttime = "7:30 AM"#raw_input("starttime 7:30 AM:")
-    endtime = "10:30 PM"#raw_input("endtime 8:40 PM:")
+    description = raw_input("Desc:")
+    loc = raw_input("Location:")
+    name = raw_input("Name:")
+    starttime = raw_input("starttime 7:30 AM:")
+    endtime = raw_input("endtime 8:40 PM:")
     rank = timesort(starttime)
     event = {"sorted_time": rank, "name":name, "start_time":starttime, "end_time":endtime, "location":loc, "lat":locDic[loc][0], "lng":locDic[loc][1], "date":date, "description":description, "count": 0, "id": idfixer(date,name)}
     firebase.post("/{}".format(branch),event)
